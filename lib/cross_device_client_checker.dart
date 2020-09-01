0652a924e90dc27552e88bfbf53a12c9f2b28487
Andrew Ananta <kuro.ketanitem@gmail.com>
2020-09-01 18:07:26 +0700

    change typo `import` to `export`

--- lib/cross_device_client_checker.dart
+++ lib/cross_device_client_checker.dart
@@ -2,7 +2,7 @@ library cross_device_client_checker;
 
 import 'package:cross_device_client_checker/entity/device_client_entity.dart';
 
-import 'cross_device_client_checker_stub.dart'
+export 'cross_device_client_checker_stub.dart'
   if(dart.library.io) 'native_device_client_checker.dart'
   if(dart.library.html) 'browser_device_client_checker.dart';
 
